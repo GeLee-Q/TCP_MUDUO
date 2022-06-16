7e2d043d0ddd4482ed4fef58a6dc7e3e2107751c
GeLee-Q <865038696@qq.com>
2022-06-16 20:28:03 +0800

    Channel

---
+++ mylib/mylib/Channel.cpp
@@ -0,0 +1,73 @@
+#include "mylib/Timestamp.h"
+#include <memory>
+#include <sys/epoll.h>
+
+#include <mylib/Channel.h>
+#include <mylib/EventLoop.h>
+
+
+const int Channel::kNoneEvent = 0;
+const int Channel::kReadEvent = EPOLLIN | EPOLLPRI;
+const int Channel::kWriteEvent = EPOLLOUT;
+
+Channel::Channel(EventLoop * loop, int fd):
+    loop_(loop), fd_(fd),
+    events_(0), revents_(0),
+    index_(-1), tied_(false){}
+
+Channel::~Channel(){
+
+}
+
+
+// 当channel 所表示的fd的事件改变，
+// update将通过eventLoop更改Poller中fd对应的事件
+void Channel::update(){
+    loop_->updateChannel(this);
+}
+
+//在eventLoop中去除这个Channel
+void Channel::remove(){
+    loop_->removeChannel(this);
+}
+
+void Channel::tie(const std::shared_ptr<void> & obj){
+    tie_ = obj;
+    tied_ = true;
+}
+
+void Channel::handleEvent(Timestamp receiveTime){
+    if(tied_){
+        std::shared_ptr<void> guard = tie_.lock();
+        if(guard){
+            handleEventWithGuard(receiveTime);
+        }
+    }
+}
+
+void Channel::handleEventWithGuard(Timestamp receiveTime){
+    // tcpConnection 对应的Channel通过shutdown关闭写端 epoll触发EPOLLHUP
+    if((revents_ & EPOLLHUP) && !(revents_  & EPOLLIN)){
+        if(closeCallback_){
+            closeCallback_();
+        }
+    }
+
+    if(revents_ & EPOLLERR){
+        if(errorCallback_){
+            errorCallback_();
+        }
+    }
+
+    if(revents_ & (EPOLLIN | EPOLLPRI)){
+        if(readCallback_){
+            readCallback_(receiveTime);
+        }
+    }
+
+    if(revents_ & EPOLLOUT){
+        if(writeCallback_){
+            writeCallback_();
+        }
+    }
+}
--- mylib/mylib/Channel.h
+++ mylib/mylib/Channel.h
@@ -8,20 +8,112 @@
 
 class EventLoop;
 
-/*
-   EventLoop, Channel, Epoll 
-   将文件描述符封装成一个Channel类 
-   封装 sockfd 和相关的 event 
-   相关时间的回调函数
+/* Channel 代表 sockfd 和相关的 event 
+   
+   通过 Epoll 返回文件描述符
+   通过 EventLoop 调用Channel 执行事件
+   二者通过EventLoop 沟通
+   Channel <-- EventLoop --> Epoll
 */
 
 
 class Channel : noncopyable{
+public:
+    // 两个回调函数对象
+    using EventCallback = std::function<void()>;
+    using ReadEventCallback = std::function<void(Timestamp)>;
+
+    Channel(EventLoop * loop , int fd);
+    ~Channel();
+
+    // fd得到Epoll通知后 处理事件 handleEvent 在Loop中调用
+    void handleEvent(Timestamp receiveTime);
+
+    // 设置回调函数
+    void setReadCallback(ReadEventCallback cb){
+        readCallback_ = std::move(cb);
+    }
+
+    void setWriteCallback(EventCallback cb){
+        writeCallback_ = std::move(cb);
+    }
+
+    void setCloseCallback(EventCallback cb){
+        closeCallback_ = std::move(cb);
+    }
+
+    void setErrorCallback(EventCallback cb){
+        errorCallback_ = std::move(cb);
+    }
+    
+    // life-period control
+    void tie(const std::shared_ptr<void> &);
+
+
+    // 得到文件描述符和关心事件的类型；
+    int fd() const{
+        return fd_;
+    }
+
+    int event() const {
+        return events_;
+    }
+
+    void set_revents(int revt){
+        revents_ = revt;
+    }
+
+    // 设置fd相应事件状态 update 相当于 epoll_ctl add delelte
+    void enableReading(){ events_ |= kReadEvent; update();}
+    void disableReading(){ events_ &= ~kReadEvent; update();}
+
+    void enableWriting(){events_ |= kWriteEvent; update(); }
+    void disableWriting(){events_ &= ~kWriteEvent; update();}
+
+    void disbaleAll(){ events_ &= ~kNoneEvent; update();}
+
+    //返回fd当前事件的状态
+    bool isNoneEvent() const { return events_ == kNoneEvent ;}
+
+    bool isWriting() const {return events_ & kWriteEvent; }
+    bool isReading() const {return events_ & kReadEvent;}
+
+    int index() const { return index_; }
+    void set_index(int idx){
+        index_ = idx;
+    }
+
+    // one loop per thread
+    EventLoop * ownerloop() {
+        return loop_;
+    }
+    void remove();
+
 private:
 
-public:
-    using EventCallback = std::function<void()>;
-    using EventReadback = std::function<void(Timestamp)>;
+    void update();
+    void handleEventWithGuard(Timestamp receiveTime);
 
+    static const int kNoneEvent;
+    static const int kReadEvent;
+    static const int kWriteEvent;
+
+    EventLoop * loop_;  // 事件循环
+    const int fd_;      // Epoll 监听的文件描述符
+    int events_;        // fd 感兴趣的事件
+    int revents_;       // Epoll返回的具体事件
+    int index_;
+
+
+    // 改变弱回调的生命周期的问题
+    std::weak_ptr<void> tie_;
+    bool tied_;
+
+
+    //负责具体事件的回调
+    ReadEventCallback readCallback_;
+    EventCallback writeCallback_;
+    EventCallback closeCallback_;
+    EventCallback errorCallback_;
 
 };
