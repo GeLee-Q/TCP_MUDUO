ecfd5c443ee0971d7125620da501b291f26d8179
GeLee-Q <865038696@qq.com>
2022-06-25 17:10:50 +0800

    server_test

--- src/server_00.cpp
+++ src/server_00.cpp
@@ -1,27 +1,61 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <string.h>
+#include <string>
+
+#include <mylib/TcpServer.h>
+#include <mylib/Logger.h>
+
+class EchoServer
+{
+public:
+    EchoServer(EventLoop *loop, const InetAddress &addr, const std::string &name)
+        : server_(loop, addr, name)
+        , loop_(loop)
+    {
+        // 注册回调函数
+        server_.setConnectionCallback(
+            std::bind(&EchoServer::onConnection, this, std::placeholders::_1));
+        
+        server_.setMessageCallback(
+            std::bind(&EchoServer::onMessage, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+
+        // 设置合适的subloop线程数量
+        server_.setThreadNum(3);
+    }
+    void start()
+    {
+        server_.start();
+    }
+
+private:
+    // 连接建立或断开的回调函数
+    void onConnection(const TcpConnectionPtr &conn)   
+    {
+        if (conn->connected())
+        {
+            LOG_INFO("Connection UP : %s", conn->peerAddress().toIpPort().c_str());
+        }
+        else
+        {
+            LOG_INFO("Connection DOWN : %s", conn->peerAddress().toIpPort().c_str());
+        }
+    }
+
+    // 可读写事件回调
+    void onMessage(const TcpConnectionPtr &conn, Buffer *buf, Timestamp time)
+    {
+        std::string msg = buf->retrieveAllAsString();
+        conn->send(msg);
+        // conn->shutdown();   // 关闭写端 底层响应EPOLLHUP => 执行closeCallback_
+    }
+
+    EventLoop *loop_;
+    TcpServer server_;
+};
 
 int main() {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    struct sockaddr_in serv_addr;
-    bzero(&serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(8888);
-
-    bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));
-
-    listen(sockfd, SOMAXCONN);
-    
-    struct sockaddr_in clnt_addr;
-    socklen_t clnt_addr_len = sizeof(clnt_addr);
-    bzero(&clnt_addr, sizeof(clnt_addr));
-
-    int clnt_sockfd = accept(sockfd, (sockaddr*)&clnt_addr, &clnt_addr_len);
-
-    printf("new client fd %d! IP: %s Port: %d\n", clnt_sockfd, inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
+    EventLoop loop;
+    InetAddress addr(8002);
+    EchoServer server(&loop, addr, "EchoServer");
+    server.start();
+    loop.loop();
     return 0;
 }
